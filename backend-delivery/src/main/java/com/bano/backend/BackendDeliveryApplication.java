ec895d838b5c1032ae2482516ba5cdcca32adaf1
cabano3 <cabano3@espe.edu.ec>
2020-12-25 15:20:29 -0500

    Proyecto

---
+++ backend-delivery/src/main/java/com/bano/backend/BackendDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.bano.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeliveryApplication.class, args);
+	}
+
+}
