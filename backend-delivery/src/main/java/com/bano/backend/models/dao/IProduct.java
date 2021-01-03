cfa6f2c25eeac042d1949b3ac646fccf07e309e9
cabano3 <cabano3@espe.edu.ec>
2021-01-03 11:51:02 -0500

    Limpiando la rama

--- backend-delivery/src/main/java/com/bano/backend/models/dao/IProduct.java
+++
@@ -1,9 +0,0 @@
-package com.bano.backend.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bano.backend.models.entities.Product;
-
-public interface IProduct extends CrudRepository<Product, Long>{
-
-}
--- backend-delivery/src/main/java/com/bano/backend/models/dao/IUser.java
+++
@@ -1,9 +0,0 @@
-package com.bano.backend.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bano.backend.models.entities.User;
-
-public interface IUser extends CrudRepository<User, Long>{
-
-}
